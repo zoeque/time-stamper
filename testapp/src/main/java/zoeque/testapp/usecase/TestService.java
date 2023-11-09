7c3ca0683f9fff99ae6223d8ae7315fd9162fdd8
zoeque <121230485+zoeque@users.noreply.github.com>
2023-11-10 07:28:35 +0900

    #14 - modified the name of the service

--- testapp/src/main/java/zoeque/testapp/usecase/TestService.java
+++ testapp/src/main/java/zoeque/testapp/usecase/TestService.java
@@ -2,22 +2,22 @@ package zoeque.testapp.usecase;
 
 import io.vavr.control.Try;
 import org.springframework.stereotype.Service;
-import zoeque.stamper.usecase.service.CertificationCreatorService;
+import zoeque.stamper.usecase.service.TimeStampCreatorService;
 
 /**
  * Create new timestamp of given file
  */
 @Service
 public class TestService {
-  CertificationCreatorService certificationCreatorService;
+  TimeStampCreatorService timeStampCreatorService;
 
-  public TestService(CertificationCreatorService certificationCreatorService) {
-    this.certificationCreatorService = certificationCreatorService;
+  public TestService(TimeStampCreatorService timeStampCreatorService) {
+    this.timeStampCreatorService = timeStampCreatorService;
   }
 
   public Try<String> execute(String file) {
     try {
-      Try<String> certificateTry = certificationCreatorService.createCertificate(file);
+      Try<String> certificateTry = timeStampCreatorService.createCertificate(file);
       return Try.success(certificateTry.get());
     } catch (Exception e) {
       return Try.failure(e);

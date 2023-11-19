37205c154c0479c5fb9bf3213305f5b97cf52375
zoeque <121230485+zoeque@users.noreply.github.com>
2023-11-19 13:06:40 +0900

    #23 - Adapter implements the method defined the interface

--- lib/src/main/java/zoeque/stamper/adapter/IFileHandleAdapter.java
+++ lib/src/main/java/zoeque/stamper/adapter/IFileHandleAdapter.java
@@ -5,6 +5,12 @@ import io.vavr.control.Try;
 /**
  * The interface for the file handling adapters.
  */
-public interface IFileHandleAdapter {
-
+public interface IFileHandleAdapter<T> {
+  /**
+   * Handle the file to read or write to the local environment.
+   *
+   * @param file The file with defined type.
+   * @return The byte converted file with the result {@link Try}.
+   */
+  Try<byte[]> handleFile(T file);
 }

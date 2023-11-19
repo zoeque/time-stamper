c5949f4b6829062b34653fa6ac47863669db63b0
zoeque <121230485+zoeque@users.noreply.github.com>
2023-11-19 16:00:17 +0900

    #23 - Get all responses and verify

--- lib/src/main/java/zoeque/stamper/usecase/service/validator/AbstractTimeStampValidateService.java
+++ lib/src/main/java/zoeque/stamper/usecase/service/validator/AbstractTimeStampValidateService.java
@@ -1,7 +1,9 @@
 package zoeque.stamper.usecase.service.validator;
 
+import io.vavr.control.Try;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.tsp.TimeStampResponse;
 import org.bouncycastle.tsp.TimeStampToken;
 import zoeque.stamper.adapter.FileReadAdapter;
@@ -14,7 +16,10 @@ import zoeque.stamper.adapter.FileReadAdapter;
 public abstract class AbstractTimeStampValidateService {
 
   FileReadAdapter readAdapter;
-  BouncyCastleProvider bouncyCastleProvider;
+
+  public AbstractTimeStampValidateService(FileReadAdapter readAdapter) {
+    this.readAdapter = readAdapter;
+  }
 
   /**
    * The validation process that {@link TimeStampResponse} is
@@ -23,22 +28,27 @@ public abstract class AbstractTimeStampValidateService {
    * and the time stamp token with the absolute path.
    *
    * @param responsePath The response file with the absolute path.
-   * @param tokenPath    The token file with the absolute path.
-   * @return Return true if the response file is valid.
+   * @return Return Instance of {@link TimeStampToken} if the response file is valid.
    */
-  public Boolean validate(String responsePath,
-                          String tokenPath) {
+  public Try<List<TimeStampToken>> validate(String responsePath) {
     try {
       // prepare the validation
-      TimeStampResponse response
-              = new TimeStampResponse(
-              readAdapter.handleFile(responsePath).get());
-      TimeStampToken token = response.getTimeStampToken();
-      return token == null ? false : true;
-
+      Try<List<String>> responses
+              = readAdapter.handleResponses(responsePath);
+      List<TimeStampToken> tokens = new ArrayList<>();
+      for (String responseDir : responses.get()) {
+        TimeStampResponse timeStampResponse
+                = new TimeStampResponse(readAdapter.handleFile(responseDir).get());
+        TimeStampToken token = timeStampResponse.getTimeStampToken();
+        tokens.add(token);
+        if (token == null) {
+          throw new IllegalArgumentException("Response is invalid!!");
+        }
+      }
+      return Try.success(tokens);
     } catch (Exception e) {
       log.warn("Cannot validate the timestamp file :{}", e.toString());
-      throw new IllegalStateException(e);
+      return Try.failure(e);
     }
   }
 }
--- testapp/src/test/java/zoeque/testapp/usecase/TimeStampServiceTest.java
+++ testapp/src/test/java/zoeque/testapp/usecase/TimeStampServiceTest.java
@@ -2,23 +2,38 @@ package zoeque.testapp.usecase;
 
 import io.vavr.control.Try;
 import java.nio.file.Paths;
+import java.util.List;
+import org.bouncycastle.tsp.TimeStampToken;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import zoeque.stamper.usecase.service.validator.TimeStampValidateService;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class TimeStampServiceTest {
   @Autowired
   TestService service;
+  @Value("${zoeque.time.stamper.artifact.directory}")
+  String artifactDirectory;
+  @Autowired
+  TimeStampValidateService validateService;
 
   @Test
   public void attemptToCreateTimeStamp_createTimestampResponseInResourceDir() {
     String path = Paths.get("src/test/resources/TestText.txt").toString();
     Try<String> executeTry = service.execute(path);
     Assertions.assertTrue(executeTry.isSuccess());
+  }
+
+  @Test
+  public void createTimeStampResponse_whenAttemptVerify_thenReturnTokenWithValidParameters() {
+    attemptToCreateTimeStamp_createTimestampResponseInResourceDir();
+    Try<List<TimeStampToken>> validateTry = validateService.validate(artifactDirectory + "*.tsr");
+    Assertions.assertTrue(validateTry.isSuccess());
   }
 }
